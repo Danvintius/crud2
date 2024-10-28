b74123cfa6d62d54ae4a09ccde8505226c99a4c9
Azerty <danila.94@mail.ru>
2024-10-28 23:25:19 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import Updates from "./components/UpDates";
+import AddForm from "./components/AddForm";
+import NoteList from "./components/NoteList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [notes, setNotes] = useState([]);
+
+    const getAllNotes = () => {
+        fetch(process.env.REACT_APP_CRUD_URL)
+            .then((response) => response.json())
+            .then((notes) => {
+                setNotes(notes);
+            });
+    };
+
+    const addNote = (note) => {
+        fetch(process.env.REACT_APP_CRUD_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(note),
+        }).then(() => getAllNotes());
+    };
+
+    const deleteNote = (id) => {
+        fetch(`${process.env.REACT_APP_CRUD_URL}/${id}`, {
+            method: "DELETE",
+        }).then(() => getAllNotes());
+    };
+
+    return (
+        <div className="Crud">
+            <Updates getAllNotes={getAllNotes} />
+            <NoteList notes={notes} deleteNote={deleteNote} />
+            <AddForm addNote={addNote} />
+        </div>
+    );
 }
 
 export default App;
